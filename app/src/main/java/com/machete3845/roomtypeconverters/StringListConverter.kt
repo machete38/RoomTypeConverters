195b9ee4366a09d836414d4caa633a467b9ade3b
machete38 <machete384@icloud.com>
2025-05-25 21:02:18 +0300

    String list converter

---
+++ app/src/main/java/com/machete3845/roomtypeconverters/StringListConverter.kt
@@ -0,0 +1,22 @@
+package com.machete3845.roomtypeconverters
+
+import androidx.room.TypeConverter
+import java.lang.reflect.Type
+
+object StringListConverter {
+    @TypeConverter
+    fun fromString(value: String?): List<String> {
+        if (value == null) {
+            return emptyList()
+        }
+        val listType: Type = object : TypeToken<List<String>>() {}.type
+        return Gson().fromJson(value, listType)
+    }
+
+    @TypeConverter
+    fun fromList(list: List<String>?): String? {
+        return list?.let {
+            Gson().toJson(it)
+        }
+    }
+}
